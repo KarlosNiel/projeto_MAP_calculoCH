e163aa4bfd04f6841afb3984ba8019b5308b5339
Carlos Daniel <144691269+KarlosNiel@users.noreply.github.com>
2025-05-26 10:34:15 -0300

    algumas remodelagens e mudanças no cod

--- projeto_map/sistema_ch/admin.py
+++ projeto_map/sistema_ch/admin.py
@@ -1,29 +1,58 @@
 from django.contrib import admin
-from .models import CustomUser, Atividade, Submissao, Notificacao
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group, Permission
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from django.core.exceptions import PermissionDenied
+from django.contrib import messages
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'tipo', 'horas_totais')
-    list_filter = ('tipo',)
-    search_fields = ('username', 'email')
-    readonly_fields = ('created_at', 'updated_at')
+from .models import User, Atividade, Submissao, Notificacao
 
+# Admin para User personalizado
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Informações adicionais', {'fields': ('horas_totais',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'horas_totais', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+# Admin para Atividade
 @admin.register(Atividade)
 class AtividadeAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'tipo', 'horas')
+    list_display = ('nome', 'tipo', 'horas', 'created_at', 'deleted_at')
+    search_fields = ('nome', 'tipo')
     list_filter = ('tipo',)
-    search_fields = ('nome',)
     readonly_fields = ('created_at', 'updated_at')
 
+# Admin para Submissao com lógica de aprovação
 @admin.register(Submissao)
 class SubmissaoAdmin(admin.ModelAdmin):
-    list_display = ('aluno', 'atividade', 'status', 'created_at')
+    list_display = ('id', 'aluno', 'atividade', 'status', 'created_at')
     list_filter = ('status',)
     search_fields = ('aluno__username', 'atividade__nome')
     readonly_fields = ('created_at', 'updated_at')
+    actions = ['aprovar_submissoes']
 
+    def aprovar_submissoes(self, request, queryset):
+        aprovadas = 0
+        erros = 0
+        for submissao in queryset:
+            try:
+                submissao.aprovar(request.user)  # usa seu método de aprovação
+                aprovadas += 1
+            except PermissionDenied as e:
+                erros += 1
+        if aprovadas:
+            self.message_user(request, f'{aprovadas} submissão(ões) aprovada(s) com sucesso.', messages.SUCCESS)
+        if erros:
+            self.message_user(request, f'{erros} submissão(ões) não puderam ser aprovadas.', messages.ERROR)
+    aprovar_submissoes.short_description = "Aprovar submissões selecionadas"
+
+# Admin para Notificação
 @admin.register(Notificacao)
 class NotificacaoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'mensagem', 'created_at')
-    search_fields = ('user__username', 'mensagem')
+    list_display = ('aluno', 'submissao', 'mensagem', 'created_at')
+    search_fields = ('aluno__username', 'mensagem')
     readonly_fields = ('created_at', 'updated_at')
--- projeto_map/sistema_ch/models.py
+++ projeto_map/sistema_ch/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.core.validators import MaxValueValidator
 from django.core.exceptions import PermissionDenied
-from django.contrib.auth.models import AbstractUser, Permission, Group
+from django.contrib.auth.models import AbstractUser
 from .calculateCH import *
 
 # Create your models here.
@@ -32,21 +32,10 @@ class BaseModel(models.Model):
     def hard_delete(self):
         super().delete()
 
+class User(AbstractUser):
+    horas_totais = models.PositiveIntegerField(default=0)
 
-class CustomUser(BaseModel, AbstractUser):
-    USER_TYPE = [
-        ('SECRETARIO', 'Secretário'),
-        ('ALUNO', 'Aluno'),
-    ]
-    tipo = models.CharField(max_length=20, choices=USER_TYPE, default='ALUNO')
-    horas_totais = models.PositiveIntegerField(default=0, validators = [MaxValueValidator(80)])
 
-    def is_secretario(self):
-        return self.tipo == 'SECRETARIO'
-    
-    groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name='customuser_groups',related_query_name='customuser')
-    user_permissions = models.ManyToManyField(Permission, verbose_name='user permissions', blank=True, related_name='customuser_permissions', related_query_name='customuser')
-    
 class Atividade(BaseModel):
     ATIVIDADE_TYPE = [
         ('WORKSHOP', 'Workshop'),
@@ -67,37 +56,28 @@ class Atividade(BaseModel):
     def get_calculate_ch(self):
         return self.CALCULATE_CH_TYPE[self.tipo]
 
-class Notificacao(BaseModel):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notificacoes')
-    mensagem = models.TextField()
-
 class Submissao(BaseModel):
     STATUS_TYPE = [
         ('PENDENTE', 'Pendente'),
         ('APROVADO', 'Aprovado'),
         ('REPROVADO', 'Reprovado'),
     ]
-    aluno = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='submissoes')
+    aluno = models.ForeignKey(User, on_delete=models.CASCADE, related_name='submissoes')
     atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE, related_name='submissoes')
     status = models.CharField(max_length=20, choices=STATUS_TYPE, default='PENDENTE')
-    notificacao = models.ForeignKey(Notificacao, on_delete=models.CASCADE, related_name='submissoes', null=True, blank=True)
 
-    def aprovar(self, secretario: CustomUser):
-        if not secretario.is_secretario():
-            raise ValueError("Apenas secretários podem aprovar submissões.")
-        
+    def aprovar(self, secretario: User):
+        if self.status != 'APROVADO':
+            self.status = 'APROVADO'
+
         calculate = self.atividade.get_calculate_ch()
         horas_calculadas = calculate.calculateCH(self.atividade)
 
-        self.status = 'APROVADO'
         self.aluno.horas_totais += horas_calculadas
         self.aluno.save()
         self.save()
 
-    def save(self, *args, **kwargs):
-        if self.pk:
-            old_status = Submissao.objects.get(pk=self.pk).status
-            if self.status == 'APROVADO' and old_status != 'APROVADOR':
-                raise PermissionDenied("Use o método `aprovar()` para validar submissões!")
-            
-        super().save(*args, **kwargs)
+class Notificacao(BaseModel):
+    aluno = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notificacoes')
+    submissao = models.ForeignKey(Submissao, on_delete=models.CASCADE, related_name='notificacoes', null=True, blank=True)
+    mensagem = models.TextField()
